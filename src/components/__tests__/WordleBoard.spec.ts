9b9335f2bcefc1967a617670f7ea7aae3372df20
heather-irene <cunningham.heatherirene@gmail.com>
2024-03-23 17:35:05 -0400

    heather1stBr: Tried using vitest globals for imports, but can't get it to work

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,5 +1,7 @@
+// Set this in globals:true in test obj in vitest.config.ts
+// And, in tsconfig.vitest.json, added "vitest/globals" in "compilerOptions" obj in "types" List
+// But can't get to work
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
