b700e3a0ab6c4b6a939c04cf8dd0c75e9f9e7dad
heather-irene <cunningham.heatherirene@gmail.com>
2024-04-27 13:28:34 -0400

    buttonsBr: Starting to create GenericButton.vue component

---
+++ src/components/GenericButton.vue
@@ -0,0 +1,28 @@
+<template>
+	<button @click="handleClick" :class="buttonClass">
+		{{ props.buttonLabel }}
+	</button>
+</template>
+
+<script lang="ts" setup>
+import { defineEmits, defineProps } from 'vue'
+
+const props = defineProps(
+	{
+		buttonLabel: {
+			type: String,
+			required: true,
+		},
+		buttonClass: {
+			type: String,
+			default: '',
+		},
+	},
+);
+
+const emit  = defineEmits(['clicked']);
+
+const handleClick = () => {
+	emit('clicked');
+};
+</script>
